b87ed019588b0a77a0cc6e4a86f824df65072f7d
Farouq Mustapha <farouq.mustapha@gmail.com>
2017-05-04 01:30:30 +0800

    Add current location into emergency SMS message.

--- app/src/main/java/farouqmustapha/heartratemonitor/EmergencyCallActivity.java
+++ app/src/main/java/farouqmustapha/heartratemonitor/EmergencyCallActivity.java
@@ -15,13 +15,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.IntentSender.SendIntentException;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.LocationListener;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class EmergencyCallActivity extends AppCompatActivity {
+import static com.google.android.gms.common.api.GoogleApiClient.*;
+
+public class EmergencyCallActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private ImageButton buttonSend;
     private EditText textPhoneNo, textSMS;
+    String _latitude, _longitude;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
     private int mLastEvent = MotionEvent.ACTION_UP;
 
     private FirebaseAuth auth;
@@ -38,31 +77,49 @@ public class EmergencyCallActivity extends AppCompatActivity {
         buttonSend = (ImageButton) findViewById(R.id.buttonSend);
         textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
         textSMS = (EditText) findViewById(R.id.editTextSMS);
-        final Vibrator v = (Vibrator)getApplicationContext().getSystemService(getApplicationContext().VIBRATOR_SERVICE);
+        final Vibrator v = (Vibrator) getApplicationContext().getSystemService(getApplicationContext().VIBRATOR_SERVICE);
 
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        } else
+            Toast.makeText(this, "Not Connected!", Toast.LENGTH_SHORT).show();
+        
+        
         final Runnable runr = new Runnable() {
 
             @Override
             public void run() {
                 if(mLastEvent == MotionEvent.ACTION_MOVE) {
-                    Toast.makeText(getApplicationContext(), "3 seconds reached!", Toast.LENGTH_SHORT).show();
                     // Vibrate for 0.25 seconds
                     v.vibrate(250);
-                    String phoneNo = textPhoneNo.getText().toString();
-                    String sms = textSMS.getText().toString();
+                    if(!_latitude.isEmpty()&&!_longitude.isEmpty()) {
+                        String phoneNo = textPhoneNo.getText().toString();
+                        String sms = textSMS.getText().toString();
+                        //google.com/maps/?q=<lat>,<long>
+                        sms = sms + "\nMy Current Location : " + "google.com/maps/?q=" + _latitude + "," + _longitude;
+                        sms = sms + "\n\nThis message is sent using Pulse Keeper application.";
 
-                    try {
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage(phoneNo, null, sms, null, null);
-                        Toast.makeText(getApplicationContext(), "SMS Sent!",
-                                Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(),
-                                "SMS faild, please try again later!",
-                                Toast.LENGTH_LONG).show();
-                        e.printStackTrace();
+                        try {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                                    Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "SMS failed, please try again later!",
+                                    Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
                     }
+                    else
+                        Toast.makeText(getApplicationContext(), "Failed to get current location!", Toast.LENGTH_LONG).show();
                 }
             }
         };
@@ -92,6 +149,146 @@ public class EmergencyCallActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        settingRequest();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "Connection Suspended!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT).show();
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, 90000);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i("Current Location", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    /*Method to get the enable location settings dialog*/
+    public void settingRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);    // 10 seconds, in milliseconds
+        mLocationRequest.setFastestInterval(1000);   // 1 second, in milliseconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can
+                        // initialize location requests here.
+                        getLocation();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(EmergencyCallActivity.this, 1000);
+                        } catch (SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
+        switch (requestCode) {
+            case 1000:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        // All required changes were successfully made
+                        getLocation();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        // The user was asked to change settings, but chose not to
+                        Toast.makeText(this, "Location Service not Enabled", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+    }
+
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        } else {
+            /*Getting the location after aquiring location service*/
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+//            if (mLastLocation != null) {
+//                _progressBar.setVisibility(View.INVISIBLE);
+//                _latitude.setText("Latitude: " + String.valueOf(mLastLocation.getLatitude()));
+//                _longitude.setText("Longitude: " + String.valueOf(mLastLocation.getLongitude()));
+//            } else {
+//                /*if there is no last known location. Which means the device has no data for the loction currently.
+//                * So we will get the current location.
+//                * For this we'll implement Location Listener and override onLocationChanged*/
+//                Log.i("Current Location", "No data for location found");
+
+            if (!mGoogleApiClient.isConnected())
+                mGoogleApiClient.connect();
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+//            }
+        }
+    }
+
+    /*When Location changes, this method get called. */
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        _latitude = Double.toString(mLastLocation.getLatitude());
+        _longitude = Double.toString(mLastLocation.getLongitude());
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
